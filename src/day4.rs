e6ac0e440f26c3a583a465d344e0c19a92a1007e
Brage Vik <brage.vik@lyse.net>
2025-11-01 23:37:24 +0100

    feat: complete day 4 task 2

--- src/day4.rs
+++ src/day4.rs
@@ -51,21 +51,19 @@ fn check_marks(x: usize, y: usize, board: &Board) -> bool {
         || (0..BOARD_HEIGHT).all(|col_y| board.marks[col_y][x])
 }
 
-fn update_and_check_board(number: u8, boards: &mut Vec<Board>) -> Option<&Board> {
-    for board in boards {
-        'board: for (y, row) in board.numbers.iter().enumerate() {
-            for (x, num) in row.iter().enumerate() {
-                if *num == number {
-                    board.marks[y][x] = true;
-                    if check_marks(x, y, board) {
-                        return Some(board);
-                    }
-                    break 'board;
+fn update_board_and_check_board(number: u8, board: &mut Board) -> bool {
+    for (y, row) in board.numbers.iter().enumerate() {
+        for (x, num) in row.iter().enumerate() {
+            if *num == number {
+                board.marks[y][x] = true;
+                if check_marks(x, y, board) {
+                    return true;
                 }
+                return false;
             }
         }
     }
-    None
+    false
 }
 
 fn calculate_board_score(board: &Board, number: u8) -> u64 {
@@ -91,8 +89,10 @@ where
 {
     let (sequence, mut boards) = get_sequence_and_board(input);
     for num in sequence {
-        if let Some(winner) = update_and_check_board(num, &mut boards) {
-            return calculate_board_score(winner, num).to_string();
+        for board in &mut boards {
+            if update_board_and_check_board(num, board) {
+                return calculate_board_score(board, num).to_string();
+            }
         }
     }
     "No winners".to_string()
@@ -103,5 +103,23 @@ where
     I: Iterator<Item = Result<String, E>>,
     E: Debug,
 {
-    todo!("Task not implemented");
+    let (sequence, mut boards) = get_sequence_and_board(input);
+    let mut last_board = None;
+    let mut last_num_i = 0;
+    for board in &mut boards {
+        for (i, num) in sequence.iter().enumerate() {
+            if update_board_and_check_board(*num, board) {
+                if i > last_num_i {
+                    last_board = Some(board);
+                    last_num_i = i;
+                }
+                break;
+            }
+        }
+    }
+
+    match last_board {
+        None => "No winners".to_string(),
+        Some(last_winner) => calculate_board_score(last_winner, sequence[last_num_i]).to_string(),
+    }
 }
