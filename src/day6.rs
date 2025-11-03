3c51dfcabc56371dedef576dc36047523a914281
Brage Vik <brage.vik@lyse.net>
2025-11-03 11:31:05 +0100

    perf: change memo in day 6 from hashmap to vector

--- src/day6.rs
+++ src/day6.rs
@@ -1,26 +1,36 @@
-use std::{collections::HashMap, fmt::Debug};
+use std::fmt::Debug;
 
-fn total_number_of_fish(counter: u8, time: u16, memo: &mut HashMap<(u8, u16), u64>) -> u64 {
+const NEW_FISH_COUNTER: usize = 8;
+const RESET_CONTER: usize = 6;
+
+fn to_index(counter: usize, time: usize) -> usize {
+    (time - 1) * NEW_FISH_COUNTER + counter
+}
+
+fn total_number_of_fish(counter: usize, time: usize, memo: &mut [Option<u64>]) -> u64 {
     if time == 0 {
         return 1;
     }
-    if let Some(count) = memo.get(&(counter, time)) {
-        return *count;
+    if let Some(count) = memo[to_index(counter, time)] {
+        return count;
     }
     let count = match counter {
-        0 => total_number_of_fish(6, time - 1, memo) + total_number_of_fish(8, time - 1, memo),
+        0 => {
+            total_number_of_fish(RESET_CONTER, time - 1, memo)
+                + total_number_of_fish(NEW_FISH_COUNTER, time - 1, memo)
+        }
         1.. => total_number_of_fish(counter - 1, time - 1, memo),
     };
-    memo.insert((counter, time), count);
+    memo[to_index(counter, time)] = Some(count);
     count
 }
 
-fn solve_tasks<I, E>(mut input: I, time: u16) -> String
+fn solve_tasks<I, E>(mut input: I, time: usize) -> String
 where
     I: Iterator<Item = Result<String, E>>,
     E: Debug,
 {
-    let mut memo = HashMap::new();
+    let mut memo = vec![None; NEW_FISH_COUNTER * time];
     input
         .next()
         .unwrap()
